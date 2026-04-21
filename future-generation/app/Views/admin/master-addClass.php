b30b150aff1a348e7cfe385a4e2918960db48142
RoshitaKaushik <roshita.kaushik@akalinfo.com>
2026-04-21 11:09:04 +0530

    Remove legacy future-generation module completely from repository

--- future-generation/app/Views/admin/master-addClass.php
+++
@@ -1,1 +0,0 @@
-<h1>'MASTER ADD-CLASS' VIEW</h1>
--- future-generation/app/Views/email_transcript.php
+++
@@ -1,2 +0,0 @@
-<p> Transcript Details: </p>
-<p> Student Name:<?=$student_name?> <br> Grade Name:<?=$grade?> <br> Course Date:<?=$semester?>  <br> Course Name :<?=$CourseName?> <br> </p>
